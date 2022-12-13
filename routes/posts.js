6a6ed6a699f90967946defeeca4ee96cf12a55c4
jeysneha <jeysneha1@gmail.com>
2022-12-13 16:35:55 -0500

    joseph changes

--- routes/posts.js
+++ routes/posts.js
@@ -3,4 +3,55 @@ const router = express.Router();
 const data = require('../data');
 const {ObjectId} = require('mongodb');
 const postData = data.posts;
+const path = require('path');
 //test
+router
+  .route('/products')
+  .get(async (req, res) => {
+    //code here for GET
+    try{
+        const prList=await postData.getAllPosts();
+        return res.status(200).render('list',{productarray:prList})
+    }
+    catch(e){
+        res.status(404).render('error',{ti: "Error Page", class: "error", message: "No Products to Display" });
+
+    }
+})
+
+router
+  .route('/:postId')
+  .get(async (req, res) => {
+    //code here for GET
+    try {
+      req.params.postId = checkId(req.params.postId);
+    } catch (e) {
+      return res.status(400).render('error',{error: e});
+    }
+    try {
+      const single_post = await postData.getPostById(req.params.movieId);
+      return res.status(200).render('details',{prdobj:single_post});
+    } catch (e) {
+      return res.status(404).render('error',{ti: "Error Page",error: 'Post Not found'});
+    }
+  })
+
+  router
+  .route('/productreg')
+  .post(async (req, res) => {
+    //code here for POST
+    let userInfo = req.body;
+    if (!userInfo.title||!userInfo.body||!userInfo.imgfiles||!userInfo.category||!userInfo.tradeStatus||!userInfo.posterId)
+    {
+      return res.status(400).json({ error: 'the request body is not valid' });
+    }
+    try {
+        const cpost=await postData.createPost(userInfo.title,userInfo.body,userInfo.imgfiles,userInfo.category,userInfo.tradeStatus,userInfo.posterId)
+        
+        return res.status(200).render('details',{prdobj:cpost});
+
+    } catch (e) {
+        return res.status(404).render('error',{ti: "Error Page",error: 'Post Not found'});
+      }
+    })
+
